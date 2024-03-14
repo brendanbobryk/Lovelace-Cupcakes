0f1d01d033f9cb43c714f010ecbb84a8ce90400e
Brendan Bobryk <babobryk@gmail.com>
2024-03-13 22:31:27 -0700

    Adjusted file pathing

--- demo/src/main/java/com/example/BubbleSort.java
+++ demo/src/main/java/com/example/BubbleSort.java
@@ -7,9 +7,7 @@ public class BubbleSort {
   private static int count = 0;
 
   public static void main(String[] args) {
-    String fileName = "/Users/jerom/Documents/GitHub/class-java/sort/demo/src/main/java/com/sort/cupcake_3906.json";
-    // String fileName =
-    // "/Users/jerom/Documents/GitHub/class-java/sort/demo/src/main/java/com/sort/cupcake_10.json";
+    String fileName = "C:/Users/Brendan/Documents/GitHub/Lovelace-Cupcakes/demo/src/main/java/com/example/cupcake_test_10.json";
 
     // read cupcake names
     JSONArray cupcakeArray = JSONFile.readArray(fileName);
