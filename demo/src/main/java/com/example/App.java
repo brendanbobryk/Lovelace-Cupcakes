1e44c526a312798c19775353cddca4d34428ce7b
Brendan Bobryk <babobryk@gmail.com>
2024-03-13 22:50:59 -0700

    Used example code within App.java and implemented insertion sort algorithm

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,82 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.json.simple.*;
+
+public class App {
+
+    private static int count = 0;
+
+    public static void main(String[] args) {
+        String fileName = "C:/Users/Brendan/Documents/GitHub/Lovelace-Cupcakes/demo/src/main/java/com/example/cupcake_test_10.json";
+
+        // read cupcake names
+        JSONArray cupcakeArray = JSONFile.readArray(fileName);
+        String[] cupcakeNameArray = nameArray(cupcakeArray);
+        System.out.println(cupcakeNameArray);
+
+        // print unsorted list
+        System.out.println("----- Unsorted array -----");
+        print(cupcakeNameArray);
+
+        // sort
+        insertionSort(cupcakeNameArray);
+
+        // print sorted list
+        System.out.println("----- Sorted array----- ");
+        print(cupcakeNameArray);
+
+        // print statistics
+        System.out.println("----- Statistics -----");
+        System.out.printf("Size of array = %d\n", cupcakeNameArray.length);
+        System.out.printf("Count = %d\n", count);
+    }
+
+    // print cupcake array
+    public static void print(String[] cupcakeNameArray) {
+        System.out.printf("Number\tName\n");
+        System.out.printf("------\t---------------\n");
+        for (int i = 0; i < cupcakeNameArray.length; i++) {
+            System.out.printf("%04d\t%s\n", i, cupcakeNameArray[i]);
+        }
+    }
+
+    // get array of cupcake names
+    public static String[] nameArray(JSONArray cupcakeArray) {
+        String[] arr = new String[cupcakeArray.size()];
+
+        // get names from json object
+        for (int i = 0; i < cupcakeArray.size(); i++) {
+            JSONObject o = (JSONObject) cupcakeArray.get(i);
+            String name = (String) o.get("name");
+            arr[i] = name;
+        }
+        return arr;
+    }
+
+    // insertion sort array, O(n^2)
+    public static void insertionSort(String arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            String key = arr[i];
+            int j = i - 1;
+
+            /*
+             * Move elements of arr[0..i-1], that are greater than key,
+             * to one position ahead of their current position
+             */
+            while (j >= 0 && arr[j].compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // swap
+    public static void swap(String[] arr, int a, int b) {
+        String temp;
+        temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
